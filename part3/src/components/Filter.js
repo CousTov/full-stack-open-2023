5991dc33fa7a5cac2f4c4b75b5cacacfbd076256
CousTov <coustov@gmail.com>
2023-08-19 20:52:59 +0545

    The-Phonebook Frontend

---
+++ part3/src/components/Filter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Filter = ({handleFilterChange}) => {
+    return(
+        <div>
+            Filter shown with: <input onChange={handleFilterChange}/>
+        </div>
+    )
+}
+export default Filter
