54af3938e237a71e320a8146bfc78947a3bcb1f6
CousTov <coustov@gmail.com>
2023-09-11 19:59:40 +0545

    uibuild

--- part3/the-phonebook-backend/mongo.js
+++ part3/the-phonebook-backend/mongo.js
@@ -16,7 +16,7 @@ if (argsLen != 3 && argsLen !== 5){
 
 const pass = process.argv[2]
 
-const url = `mongodb+srv://fullstackopen:${password}@phonebookdb.kpwkwxw.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstackopen:${pass}@phonebookdb.kpwkwxw.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
