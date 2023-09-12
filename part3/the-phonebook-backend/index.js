6812aef6fdfa24bb168d3fd84aaf3a6a0062c67b
CousTov <coustov@gmail.com>
2023-09-12 06:51:05 +0545

    Data is fetched from mongoDB now

--- part3/the-phonebook-backend/index.js
+++ part3/the-phonebook-backend/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -14,26 +17,7 @@ morgan.token('body', req => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
+
 ]
 
 app.get('/api/persons', (request, response) => {
@@ -90,7 +74,7 @@ app.post('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
